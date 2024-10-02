a98ca5406553800f39fb3152a00f20b8c1a14166
Matheus Paiva <emanuelmatheus948@gmail.com>
2024-10-02 19:50:47 -0300

    feat: adding initialization app file

---
+++ src/main/java/com/faculdade/gerenciamento_pessoas/GerenciamentoPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.gerenciamento_pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoPessoasApplication.class, args);
+	}
+
+}
